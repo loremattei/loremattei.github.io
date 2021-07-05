0a8ad0278fc9d92cafc0bcaa946d46fb1e040411
loremattei <loremattei@users.noreply.github.com>
2021-07-05 20:50:26 +0000

    deployed via Gatsby Publish Action 🎩 for 5c296084662cbacca003d7919e67e879d15c993b

---
+++ cb1608f2-39169947febe459beb1a.js
@@ -0,0 +1,3 @@
+/*! For license information please see cb1608f2-39169947febe459beb1a.js.LICENSE.txt */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{wHSu:function(a,n,i){"use strict";i.d(n,"a",(function(){return o}));var o={prefix:"fas",iconName:"shapes",icon:[512,512,[],"f61f","M128,256A128,128,0,1,0,256,384,128,128,0,0,0,128,256Zm379-54.86L400.07,18.29a37.26,37.26,0,0,0-64.14,0L229,201.14C214.76,225.52,232.58,256,261.09,256H474.91C503.42,256,521.24,225.52,507,201.14ZM480,288H320a32,32,0,0,0-32,32V480a32,32,0,0,0,32,32H480a32,32,0,0,0,32-32V320A32,32,0,0,0,480,288Z"]}}}]);
+//# sourceMappingURL=cb1608f2-39169947febe459beb1a.js.map
